775019f470aa38eca1993faf9dcc091eb0b29072
Hasan (RangerMerah) <44793389+disaster007@users.noreply.github.com>
2018-12-15 10:18:13 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,41 +1,59 @@
-print("Selamat Datang")
+print("Program Inventory")
 
-ans=True
-
-while ans:
+def menu():
 	print("""
-	1.Masukan jumlah stock barang: 
-	2.Masukan jumlah barang terjual: 
-	3.Check stock barang
-	4.keluar
-	""")
-	ans = input("Masukan Perintah Anda: ")
-	if ans=="1":
-		item_1 = int(input("Jumlah stock barang LS 155:	"))
-		item_2 = int(input("Jumlah stock barang CB 434:	"))
-		item_3 = int(input("Jumlah stock barang CB 449:	"))
-		item_4 = int(input("Jumlah stock barang CB 494:	"))
-		item_5 = int(input("Jumlah stock barang N5 150:	"))
-	elif ans=="2":
-		sold_1 = int(input("Jumlah stock barang LS 155:	"))
-		sold_2 = int(input("Jumlah stock barang CB 434:	"))
-		sold_3 = int(input("Jumlah stock barang CB 449:	"))
-		sold_4 = int(input("Jumlah stock barang CB 494:	"))
-		sold_5 = int(input("Jumlah stock barang N5 150:	"))
-	elif ans=="3":
-		rest_1 = (item_1-sold_1)
-		rest_2 = (item_2-sold_2)
-		rest_3 = (item_3-sold_3)
-		rest_4 = (item_4-sold_4)
-		rest_5 = (item_5-sold_5)
-		print("LS 155: ",rest_1)
-		print("CB 434: ",rest_2)
-		print("CB 449: ",rest_3)
-		print("CB 494: ",rest_4)
-		print("N5 150: ",rest_5)
-	elif ans=="4":
-		print("Alhamdulillah")
-		exit()
+		   Harjuna 	  Kacamata
+		============Menu=============
+		Tekan 1 : Masukan Barang Baru
+		Tekan 2 : Tambah Stock Barang
+		Tekan 3 : Check Stock Barang
+		Tekan 4 : Barang Terjual
+		Tekan q : Keluar
+		=============================
+		""")
+	return str(input("Masukan pilihan anda: "))
+
+
+
+stock ={'LS 155':0,'CB 434':0,'CB 449':0,'CB 494':0,'NS 150':0}
+
+
+while True:
+	run = menu()
+	if run == '1':
+		
+		file = open("Database.txt","a")
+		
+		addStock = input("Masukan Barang baru: ")
+		jumlah = int(input("Jumlah barang masuk: "))
+		stock[addStock] = jumlah
+
+		file.write("{}".format(addStock) + ": {}\n".format(jumlah))
+		file.close()
+
+	elif run =='2':
+		print("Stock yang akan ditambah?")
+		for key in stock.items():
+			print("{}".format(key))
+		item = input("Masukan Barang yang akan ditambah:")
+		stock[item] += int(input("Masukan  Jumlah barang: "))	
+
+	elif run == '3':
+		for key, value in stock.items():
+			print("{} tersisa {}".format(key, value)) 
+
+	elif run == '4':
+		item = input("Barang yang terjual: ")
+		if stock[item] > 0:
+			stock[item] -= int(input("Terjual: "))
+			print("{} tersisa {}".format(item, stock[item]))
+		else:
+			print("stock barang {} habis".format(item))
+	
+	elif run == 'q':
+		break
 
 	else:
-		print("Masukan nomor perintah yang sesuai!")
+		print("Masukan Pilihan yang tepat!")
+
+print("Terima Kasih")
